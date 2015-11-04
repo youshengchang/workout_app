ce4528cdc8d3461df66a1774aa66524da75f3b44
youshengchang <changyousheng@yahoo.com>
2015-11-04 17:59:25 +0000

    add search for friends

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,10 @@ class DashboardController < ApplicationController
     @athletes = User.paginate(:page => params[:page])
    
   end
+  
+  def search
+    
+    @athletes = User.search_by_name(params[:search_name]).paginate(:page => params[:page])
+    
+  end
 end
